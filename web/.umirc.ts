93b8367396fd672c9877bb5a20b67037aa03cc42
dht5867 <dht5867@gmail.com>
2025-07-21 14:38:53 +0800

    dev

--- web/.umirc.ts
+++ web/.umirc.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   proxy: [
     {
       context: ['/api', '/v1'],
-      target: 'http://121.40.167.143:9380/',
+      target: 'http://127.0.0.1:9380/',
       changeOrigin: true,
       ws: true,
       logger: console,
